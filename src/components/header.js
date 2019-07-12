2f2629151c3183ff00b208bfbbbe1246f6e280ad
AaronW <zmtzwb@gmail.com>
2019-07-12 18:22:39 +0800

    init

---
+++ src/components/header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import '../style/header/header.css';
+import {log} from "../utils/utils";
+
+export default class Header extends React.Component{
+    constructor(props) {
+        super(props);
+        log("header", "init");
+
+        this.logo_src = 'https://upload.wikimedia.org/wikipedia/commons/8/8d/Shadowsocks_logo.png';
+    }
+
+    render() {
+        return (
+            <header className='header-wrap'>
+                <div className='brand-wrap'>
+                    <a href="/" className='logo-box'>
+                        <img src={this.logo_src} className='logo-img' alt=""/>
+                    </a>
+                    <span className='brand-text'>SSR Share</span>
+                </div>
+            </header>
+        );
+    }
+}
---
+++ src/views/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {info} from "../utils/utils";
+import ShareCard from "../components/shareCard";
+
+class Index extends React.Component{
+    constructor(props) {
+        super(props);
+        info("index init");
+
+        this.state = {
+            list: [
+                {
+                    img: 'https://img.ssyer.com/picture/new/zl/a6f98fa6ae4f89041ff5ba401303690d.png',
+                    date: new Date().toLocaleDateString(),
+                    author: '123'
+                },
+                {
+                    img: 'https://img.ssyer.com/picture/new/zl/0e508de22b94f66266ef7759faf72ef8.png',
+                    date: new Date().toLocaleDateString(),
+                    author: '123'
+                },
+                {
+                    img: 'https://img.ssyer.com/picture/new/zl/bccd2ecb8193d29e911286e3344cb8be.png',
+                    date: new Date().toLocaleDateString(),
+                    author: '123'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.list.map((item, idx) =>
+                        <ShareCard album={item.img} share_date={item.date} share_author={item.author}
+                            key={idx}
+                        />
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+export default Index;
